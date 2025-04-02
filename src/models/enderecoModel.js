3f230e1690d994ef9340ba789fa939d6c581ea7f
GabrielIwakura <gabriel.iwakura@sptech.school>
2025-04-02 12:49:47 -0300

    Finalizando fetch para endereços

--- src/models/enderecoModel.js
+++ src/models/enderecoModel.js
@@ -6,7 +6,11 @@ function cadastrar(logradouro, bairro, cidade, uf, estado, fk_agencia_endereco) 
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO endereco (logradouro, bairro, cidade, uf, estado, fk_agencia_endereco) VALUES ('${logradouro}', '${bairro}', '${cidade}', '${uf}', '${estado}', '${fk_agencia_endereco}');`;
+    INSERT INTO endereco (logradouro, bairro, cidade, uf, estado, fk_agencia_endereco) 
+    VALUES ('${logradouro}', '${bairro}', '${cidade}', '${uf}', '${estado}', '${fk_agencia_endereco}');
+`;
+
+
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }

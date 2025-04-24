61bc4b90f585abde0ce6bc5e957728ca6961ed5e
joaogcr <joao.nribeiro@sptech.school>
2025-04-24 18:08:22 -0300

    mudança no script python

--- API-Python/script_insercao.py
+++ API-Python/script_insercao.py
@@ -59,12 +59,11 @@ s3 = boto3.client(
 aws_access_key_id='',
 aws_secret_access_key='',
 aws_session_token='',
-region_name=''  
+region_name='us-east-1'  
 )
 
 #usando uma variável mac
 mac = getEnderecoMAC()
-nome_do_json = f"monitoramento_{mac}_{datetime.now().strftime("%d-%m-%Y_%Hhrs%Mmin%Ss")}.json"
 
 conn = conectar()
 cursor = conn.cursor()
@@ -128,7 +127,7 @@ while True:
     print(i, discoUso, porcentagemDisco, porcentagemCpu, ramUso, porcentagemRam, porcentagemBateria, hrCaptura)
 
     nome_bucket = 'raw-allset'
-    nome_no_s3 = f"{nome_do_json}"
+    nome_do_json = f"monitoramento_{mac}_{datetime.now().strftime("%d-%m-%Y_%Hhrs%Mmin%Ss")}.json"
 
     if i != 0 and i % 100 == 0:
         df = pd.DataFrame(dados_monitoramento)
@@ -136,12 +135,11 @@ while True:
         print(f"Salvo em {nome_do_json}")
 
         try:
-            s3.upload_file(nome_do_json, nome_bucket, nome_no_s3)
-            print(f"Arquivo '{nome_do_json}' enviado com sucesso para S3 em '{nome_no_s3}'")
+            s3.upload_file(nome_do_json, nome_bucket, nome_do_json)
+            print(f"Arquivo '{nome_do_json}' enviado com sucesso para S3 em '{nome_do_json}'")
         except Exception as e:
             print(f"Erro ao enviar para o S3: {e}")
 
-        break
             
 
     time.sleep(0)

a3679bd684bbbad51781339f7f707756628aebb5
isabelafrancisco <isabela.francisco@sptech.school>
2025-05-29 10:38:02 -0300

    fazendo inserts e arrumando o back

--- src/models/gerenteModel.js
+++ src/models/gerenteModel.js
@@ -1,11 +1,11 @@
 var database = require("../database/config");
 
-function lotesMaisOcorrencia(fkAgencia){
+function lotesMaisOcorrencia(){
     let query = `SELECT l.id_lote as 'lote', COUNT(a.id_alerta) AS 'ocorrencias' 
                     FROM lote AS l
                     JOIN carro AS c ON c.fk_lote = l.id_lote
                     JOIN alerta AS a ON a.fk_carro_macadress = c.macadress
-                    WHERE l.fk_agencia_lote = ${fkAgencia}
+                    WHERE l.fk_agencia_lote = 1
                     GROUP BY l.id_lote
                     ORDER BY ocorrencias DESC
                     LIMIT 5;`

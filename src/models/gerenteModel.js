c0ad5f84a163d2ad3354fd71e610fefc225d00d6
isabelafrancisco <isabela.francisco@sptech.school>
2025-05-29 12:14:11 -0300

    arrumando as cores da dash gerente

--- src/models/gerenteModel.js
+++ src/models/gerenteModel.js
@@ -13,6 +13,19 @@ function lotesMaisOcorrencia(){
     return database.executar(query);
 }
 
+function lotesMaisOcorrencia(){
+    let query = `SELECT l.id_lote as 'lote', COUNT(a.id_alerta) AS 'ocorrencias' 
+                    FROM lote AS l
+                    JOIN carro AS c ON c.fk_lote = l.id_lote
+                    JOIN alerta AS a ON a.fk_carro_macadress = c.macadress
+                    WHERE l.fk_agencia_lote = 1
+                    GROUP BY l.id_lote
+                    ORDER BY ocorrencias DESC
+                    LIMIT 5;`
+
+    return database.executar(query);
+}
+
  module.exports = {
     lotesMaisOcorrencia
 }

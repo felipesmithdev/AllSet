464031eb49caee6c630cc7700d23c352277368ae
joaogcr <joao.nribeiro@sptech.school>
2025-06-01 19:39:54 -0300

    inserts e tabela nova do sql

--- src/models/analistaLoteModel.js
+++ src/models/analistaLoteModel.js
@@ -44,9 +44,23 @@ function plotarGrafico2(categoria2, lote, periodo, componente2) {
     return database.executar(instrucaoSql);
 }
 
+function plotarLinha() {
+        var instrucaoSql = `
+        SELECT ${categoria2}, DATE_FORMAT(data, '%d/%m') AS data
+        FROM historicoQtdPorCategoria
+        WHERE tipo = '${componente2}' AND lote = ${parseInt(lote)}
+        ORDER BY data DESC LIMIT ${parseInt(periodo)};
+    `;
+    
+    console.log("Ta dando certo caralho")
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     calcularKpi2,
     calcularKpi3,
     plotarGrafico1,
-    plotarGrafico2
+    plotarGrafico2,
+    plotarLinha
 };
